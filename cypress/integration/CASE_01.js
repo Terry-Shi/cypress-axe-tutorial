c3a3e379c5571e7847c7d98e14feaf89a101cbc4
TerryShi <terry.shi@sap.com>
2021-06-22 18:24:18 +0800

    output to terminal

--- cypress/integration/CASE_01.js
+++ cypress/integration/CASE_01.js
@@ -7,10 +7,30 @@ describe('Login', function () {
 		
     });
 
-
-	it('checkA11y', function () {
-         
-        // Test the page at initial load
-        cy.checkA11y();
-    });
+  // Then in your test...
+  it('Logs violations to the terminal', () => {
+	//cy.checkA11y(null, null, violations => cy.task('reportA11y', violations));
+    cy.checkA11y(null, null, terminalLog);
+  })
 });
+
+// Define at the top of the spec file or just import it
+function terminalLog(violations) {
+	cy.task(
+	  'log',
+	  `${violations.length} accessibility violation${
+		violations.length === 1 ? '' : 's'
+	  } ${violations.length === 1 ? 'was' : 'were'} detected`
+	)
+	// pluck specific keys to keep the table readable
+	const violationData = violations.map(
+	  ({ id, impact, description, nodes }) => ({
+		id,
+		impact,
+		description,
+		nodes: nodes.length
+	  })
+	)
+  
+	cy.task('table', violationData)
+  }
